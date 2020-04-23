8099b4563730ad4ada944dd853ab75c5ddc0ab9c
Rajssss <sssraj.sssraj@gmail.com>
2020-04-23 16:33:29 +0530

    Updated LEDControl Project
    ->Added var guards, fixes compiler optimization bugs
    ->Made code more readable.

--- 003LEDControl/Src/main.c
+++ 003LEDControl/Src/main.c
@@ -2,8 +2,10 @@
  ******************************************************************************
  * @file           : main.c
  * @author         : Rajssss@GitHub.com
- * @brief          : Bare-Metal Programme to Control On-Board User LED (LD2) of
+ * @brief          : Bare-Metal Programe to Control On-Board User LED (LD2) of
  * 					 NEUCLEO-STM32F446RE board Mapped at PORT-A Pin-5.
+ *
+ ******************************************************************************
  */
 
 #if !defined(__SOFT_FP__) && defined(__ARM_FP)
@@ -27,23 +29,20 @@ void delayOff(uint32_t t_hold) //Delay Routine for LED Off State
 
 int main(void)
 {
-	uint32_t *pClkCtrlReg = (uint32_t* )0x40023830;   //Clock control enable Reg address for AHB1 on which GPIOA is connected
-	uint32_t *pGPIOAModeReg = (uint32_t* )0x40020000;  //GPIO mode Reg address of GPIOA
-	uint32_t *pGPIOAOutDataReg = (uint32_t* )0x40020014;   //GPIO Output Data Reg address
+	uint32_t volatile *const pClkCtrlReg       = (uint32_t* ) (0x40023800 + 0x30);  //Clock control enable Reg address for AHB1 on which GPIOA is connected
+	uint32_t volatile *const pGPIOAModeReg     = (uint32_t* ) (0x40020000 + 0x00);  //GPIOA mode Reg address
+	uint32_t volatile *const pGPIOAOutDataReg  = (uint32_t* ) (0x40020000 + 0x14);  //GPIOA Output Data Reg address
 
 	*pClkCtrlReg |= 0x00000001;  //Enable clock for GPIOA in AHB1 Bus, via Clock control Reg of AHB1 (Step 1)
 
-	//*pGPIOAModeReg &= 0xFFFFF3FF; //Clearing GPIOA pin 5 mode to 00 first (Method 1)
-	*pGPIOAModeReg &= ~(3<<10);	   //Clearing GPIOA pin 5 mode to 00 first (Method 2)
-
-	//*pGPIOAModeReg |= 0x00000400; //setting GPIOA mode pin 5 to 01->O/P (Method 1)
-	*pGPIOAModeReg |= (1<<10);     //setting GPIOA mode pin 5 to 01->O/P (Method 2)
+	*pGPIOAModeReg &= ~(3<<10);	   //Clearing GPIOA pin 5 mode to 00 first, then
+	*pGPIOAModeReg |= (1<<10);     //setting GPIOA mode pin 5 to 01->O/P
 
     while(1)
     {
-		*pGPIOAOutDataReg |= 0x0020; //Set GPIOA Pin-5 to HIGH (LD2).
+		*pGPIOAOutDataReg |= (1<<5); //Set GPIOA Pin-5 to HIGH (LD2).
 		delayOn(50000);
-		*pGPIOAOutDataReg &= 0xFFDF; //Set GPIOA Pin-5 to LOW (LD2).
+		*pGPIOAOutDataReg &= ~(1<<5); //Set GPIOA Pin-5 to LOW (LD2).
 		delayOff(500000);
     }
 }
