9dc4a0b6d96584c2df8744c882616bb636bb0afb
Rajssss <sssraj.sssraj@gmail.com>
2020-04-23 16:14:27 +0530

    Updated PinControl Project
    ->Added var guards, fixes Compiler Optimiztion bugs.
    ->Made code more readable.

--- 004PinControl/Src/main.c
+++ 004PinControl/Src/main.c
@@ -2,7 +2,9 @@
  ******************************************************************************
  * @file           : main.c
  * @author         : Rajssss@GitHub.com
- * @brief          : Bare-Metal Program for GPIO Digital Read/Input
+ * @brief          : Bare-Metal Program for GPIO Digital Read/Input.
+ * 					 This Program controls the On-board LED and a user LED on
+ * 					 GIPO PA1 based on the status of GPIO PA0.
  ******************************************************************************
  */
 
@@ -12,34 +14,33 @@
 #endif
 
 #include<stdint.h>
+#define GPIOA0Stat (uint8_t volatile const)(*pGPIOAInDataReg & 0x01) //Extracts the LSB of GPIOA I/P Data Reg ie GPIOA pin-0 status
 
 int main(void)
 {
-	uint32_t *pClkCtrlReg = (uint32_t* )0x40023830;   //Clock control enable Reg address for AHB1 on which GPIOA is connected
-	uint32_t *pGPIOAModeReg = (uint32_t* )0x40020000;  //GPIO mode Reg address of GPIOA
-	uint32_t *pGPIOAOutDataReg = (uint32_t* )0x40020014;   //GPIO Output Data Reg address
-	uint32_t *pGPIOAInDataReg = (uint32_t* )0x40020010;   //GPIO Input Data Reg address
+	uint32_t volatile *const pClkCtrlReg       =  (uint32_t* ) (0x40023800 + 0x30);  //Clock control enable Reg address for AHB1 on which GPIOA is connected
+	uint32_t volatile *const pGPIOAModeReg     =  (uint32_t* ) (0x40020000 + 0x00);  //GPIOA mode Reg address
+	uint32_t volatile *const pGPIOAOutDataReg  =  (uint32_t* ) (0x40020000 + 0x14);  //GPIOA Output Data Reg address
+	uint32_t volatile *const pGPIOAInDataReg   =  (uint32_t* ) (0x40020000 + 0x10);  //GPIOA Input Data Reg address
 
-	*pClkCtrlReg |= 0x00000001;  //Enable clock for GPIOA in AHB1 Bus, via Clock control Reg of AHB1
-
-	*pGPIOAModeReg &= ~(3<<10); //Clearing GPIOA pin 5 (LD2) mode to 00 first
-	*pGPIOAModeReg |= (1<<10); //setting GPIOA pin 5 mode to 01->O/P
-
-	*pGPIOAModeReg &= ~(0x0F); //Clearing GPIOA pin 0 and 1 to 00 (INPUT) mode
-	*pGPIOAModeReg |= (1<<2);
+	*pClkCtrlReg    |=  (0x00000001);  //Enable clock for GPIOA in AHB1 Bus, via Clock control Reg of AHB1
+	*pGPIOAModeReg  &=  ~(3<<10);      //Clearing GPIOA pin 5 (LD2) mode to 00 first
+	*pGPIOAModeReg  |=  (1<<10);       //Setting GPIOA pin 5 mode to 01->O/P
+	*pGPIOAModeReg  &=  ~(0x0F);       //Clearing GPIOA pin 0 and 1 to 00 (INPUT) mode
+	*pGPIOAModeReg  |=  (1<<2);        //Setting GPIOA pin 1 mode to 01->O/P
 
 
 	while(1)
 	{
-		if((*pGPIOAInDataReg & 0x01)) //(*pGPIOAInDataReg & 0x01) extracts the LSB ie GPIOA pin-0
+		if(GPIOA0Stat)                      //If GPIO PA0 is HIGH
 		{
-			*pGPIOAOutDataReg |= (1<<5); // Set GPIOA pin-5 HIGH
-			*pGPIOAOutDataReg |= (1<<1); // Set GPIOA pin-1 HIGH
+			*pGPIOAOutDataReg |= (1<<5);    //Set GPIOA pin-5 HIGH
+			*pGPIOAOutDataReg |= (1<<1);    //Set GPIOA pin-1 HIGH
 		}
-		else
+		else								//Else
 		{
-			*pGPIOAOutDataReg &= ~(1<<5); // Set GPIOA pin-5 LOW
-			*pGPIOAOutDataReg &= ~(1<<1); // Set GPIOA pin-1 LOW
+			*pGPIOAOutDataReg &= ~(1<<5);   //Set GPIOA pin-5 LOW
+			*pGPIOAOutDataReg &= ~(1<<1);   //Set GPIOA pin-1 LOW
 		}
 	}
 }
